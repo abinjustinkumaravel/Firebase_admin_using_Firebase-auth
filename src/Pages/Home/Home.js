57707b2956259ecc3a9aef8083606fad0082bf22
abinjustinkumaravel <abinjustinkumaravel@gmail.com>
2024-10-30 00:23:33 +0530

    logout function added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import {auth} from "../../Services/firebaseConfig";
 
-const Home = () => {
+function Home(){
+
+  const[user] =useAuthState(auth);
+
+const handleLogout =() => {
+  auth.signOut()
+  .then(()=>{
+    console.log("user signed out");
+  }).catch(error => {
+    console.error("Error",error)
+  });
+}
+
   return (
-    <div>
-      Welcome to Home
+    <div className='App'>
+      {
+        user ?(
+          <div>
+            <h2>
+              welcome, to ButterBytes Admin
+            </h2>
+            <button onClick={handleLogout}> Logout</button>
+          </div>
+        ) :(
+          <h2> you are not logged in.</h2>
+        )}
+
     </div>
-  )
+  );
 }
 
 export default Home
